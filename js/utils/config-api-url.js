536fcc00f8aa89478a78174237da97cea81c36ae
Efrain Villanueva <114648604+evillanuevam@users.noreply.github.com>
2025-03-20 16:13:01 +0100

    Update config-api-url.js

--- js/utils/config-api-url.js
+++ js/utils/config-api-url.js
@@ -1,5 +1,5 @@
 //SI SE CAMBIA LA URL POR EJEMPLO EN RENDER, SE CAMBIA AQUI Y SE APLICA AUTOMATICAMENTE A TODO EL PROYECTO
 window.CONFIG = {
     //API_BASE_URL: "https://localhost:7187"
-    API_BASE_URL: "https://upload-penn-battle-federation.trycloudflare.com"
+    API_BASE_URL: "https://bathroom-corrections-resumes-pollution.trycloudflare.com"
 };
