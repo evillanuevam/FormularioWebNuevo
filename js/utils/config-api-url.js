5cb2ad0cb56cf4c5bdda2d6f6ee5b4a8a4057d23
Efrain <villanueva.efrain@hotmail.com>
2025-03-24 13:42:04 +0100

    Eliminar todo excepto la carpeta frontend

--- js/utils/config-api-url.js
+++
@@ -1,5 +0,0 @@
-//SI SE CAMBIA LA URL POR EJEMPLO EN RENDER, SE CAMBIA AQUI Y SE APLICA AUTOMATICAMENTE A TODO EL PROYECTO
-window.CONFIG = {
-    //API_BASE_URL: "https://localhost:7187"
-    API_BASE_URL: "https://upload-penn-battle-federation.trycloudflare.com"
-};
