95713f7f8e4fbdf790aa69f06188f1ee1537f323
Efrain <villanueva.efrain@hotmail.com>
2025-03-31 09:07:59 +0200

    Actualizando URL automatica

--- inicia-todo.bat
+++ inicia-todo.bat
@@ -6,8 +6,12 @@ timeout /t 10 > nul
 echo [2/4] Ejecutando script para actualizar URL...
 powershell -ExecutionPolicy Bypass -File "C:\Users\efrain.villanueva\Documents\AENA\FORMULARIO WEB\inicia-todo.ps1"
 
-echo [3/4] Abriendo Hyper en la carpeta del backend...
-powershell -Command "Start-Process 'C:\Users\efrain.villanueva\AppData\Local\Programs\Hyper\Hyper.exe' -ArgumentList '--cwd', 'C:\Users\efrain.villanueva\Documents\AENA\FormularioWebBackendAPI', '--', 'dotnet run'"
+echo [3/4] Abriendo Hyper y ejecutando dotnet run...
+start "" "C:\Users\efrain.villanueva\AppData\Local\Programs\Hyper\Hyper.exe"
+timeout /t 3 > nul
+
+:: Simular teclas para escribir y ejecutar dotnet run
+powershell -Command "$wshell = New-Object -ComObject wscript.shell; $wshell.AppActivate('Hyper'); Start-Sleep -Milliseconds 500; $wshell.SendKeys('dotnet run{ENTER}')"
 
 echo [4/4] Proceso completo. Puedes cerrar esta ventana si lo deseas.
 pause
