59ee06cffa371f833a1229c8d07dc0bca9af970e
Efrain <villanueva.efrain@hotmail.com>
2025-03-31 08:26:57 +0200

    Actualizando URL automatica

--- inicia-todo.ps1
+++ inicia-todo.ps1
@@ -27,3 +27,7 @@ cd "C:\Users\efrain.villanueva\Documents\AENA\FORMULARIO WEB"
 cmd /c "git add ."
 cmd /c "git commit -m ""Actualizando URL automatica"""
 cmd /c "git push origin main"
+
+# Abrir Hyper en la carpeta del backend y ejecutar dotnet run
+Start-Process "C:\Users\efrain.villanueva\AppData\Local\Programs\Hyper\Hyper.exe" `
+    -ArgumentList '--new-tab', '--', 'dotnet run'
--- js/utils/config-api-url.js
+++ js/utils/config-api-url.js
@@ -2,5 +2,5 @@
 window.CONFIG = {
     //API_BASE_URL: "https://localhost:7187"  //local
     //API_BASE_URL: "https://formulariowebbackendapi.onrender.com" //url fija de Render
-    API_BASE_URL: "https://besides-millions-arrest-crude.trycloudflare.com"  //url dinamica de cloudflare
+    API_BASE_URL: "https://houses-brick-mega-flight.trycloudflare.com"  //url dinamica de cloudflare
 };
