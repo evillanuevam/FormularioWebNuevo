c7eca5a1d53bd6d2877d4fba373bab8e868bd8d7
Efrain <villanueva.efrain@hotmail.com>
2025-04-11 08:00:10 +0200

    Actualizando URL automatica

--- js/utils/config-api-url.js
+++ js/utils/config-api-url.js
@@ -2,5 +2,5 @@
 window.CONFIG = {
     //API_BASE_URL: "https://localhost:7187"  //local
     //API_BASE_URL: "https://formulariowebbackendapi.onrender.com" //url fija de Render
-    API_BASE_URL: "https://be-pavilion-cache-assistance.trycloudflare.com"  //url dinamica de cloudflare
+    API_BASE_URL: "https://domain-planet-cities-shut.trycloudflare.com"  //url dinamica de cloudflare
 };
