505ea7c8a245dd285528bef01ed11a9a6afcd861
Efrain <villanueva.efrain@hotmail.com>
2025-04-30 08:37:59 +0200

    Actualizando URL automatica

--- js/utils/config-api-url.js
+++ js/utils/config-api-url.js
@@ -2,5 +2,5 @@
 window.CONFIG = {
     //API_BASE_URL: "https://localhost:7187"  //local
     //API_BASE_URL: "https://formulariowebbackendapi.onrender.com" //url fija de Render
-    API_BASE_URL: "https://referred-population-rod-ozone.trycloudflare.com"  //url dinamica de cloudflare
+    API_BASE_URL: "https://urban-fy-current-weekly.trycloudflare.com"  //url dinamica de cloudflare
 };
