37dd17edb5c1faa5888571f2684a8ff03dcf2cd6
Efrain <villanueva.efrain@hotmail.com>
2025-05-05 08:13:32 +0200

    Actualizando URL automatica

--- js/utils/config-api-url.js
+++ js/utils/config-api-url.js
@@ -2,5 +2,5 @@
 window.CONFIG = {
     //API_BASE_URL: "https://localhost:7187"  //local
     //API_BASE_URL: "https://formulariowebbackendapi.onrender.com" //url fija de Render
-    API_BASE_URL: "https://deposits-recommendation-exists-eau.trycloudflare.com"  //url dinamica de cloudflare
+    API_BASE_URL: "https://nextel-neil-suggesting-swedish.trycloudflare.com"  //url dinamica de cloudflare
 };
