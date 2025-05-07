0d520a360af439ecc5bc6c0a2829c2d7bc75e916
Efrain <villanueva.efrain@hotmail.com>
2025-05-07 08:35:23 +0200

    Actualizando URL automatica

--- js/utils/config-api-url.js
+++ js/utils/config-api-url.js
@@ -2,5 +2,5 @@
 window.CONFIG = {
     //API_BASE_URL: "https://localhost:7187"  //local
     //API_BASE_URL: "https://formulariowebbackendapi.onrender.com" //url fija de Render
-    API_BASE_URL: "https://desirable-hunting-impacts-bosnia.trycloudflare.com"  //url dinamica de cloudflare
+    API_BASE_URL: "https://likes-race-accessory-promote.trycloudflare.com"  //url dinamica de cloudflare
 };
