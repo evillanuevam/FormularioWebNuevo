f84f08f431203afa1387322d099a535c096d9f04
Efrain <villanueva.efrain@hotmail.com>
2025-05-08 12:34:55 +0200

    Actualizando URL automatica

--- js/utils/config-api-url.js
+++ js/utils/config-api-url.js
@@ -2,5 +2,5 @@
 window.CONFIG = {
     //API_BASE_URL: "https://localhost:7187"  //local
     //API_BASE_URL: "https://formulariowebbackendapi.onrender.com" //url fija de Render
-    API_BASE_URL: "https://plaza-publicity-kim-enjoy.trycloudflare.com"  //url dinamica de cloudflare
+    API_BASE_URL: "https://intent-mart-jordan-potential.trycloudflare.com"  //url dinamica de cloudflare
 };
