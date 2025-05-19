5b6cebdec26e0238a26e26c21f38f855a3c11c35
Efrain <villanueva.efrain@hotmail.com>
2025-05-19 08:37:51 +0200

    Actualizando URL automatica

--- js/utils/config-api-url.js
+++ js/utils/config-api-url.js
@@ -2,5 +2,5 @@
 window.CONFIG = {
     //API_BASE_URL: "https://localhost:7187"  //local
     //API_BASE_URL: "https://formulariowebbackendapi.onrender.com" //url fija de Render
-    API_BASE_URL: "https://come-travesti-cyprus-double.trycloudflare.com"  //url dinamica de cloudflare
+    API_BASE_URL: "https://brazil-claimed-villas-promoted.trycloudflare.com"  //url dinamica de cloudflare
 };
