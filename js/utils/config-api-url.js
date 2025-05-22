3904fbdd48e8a915cf0f90c0d17c7cecf5363321
Efrain <villanueva.efrain@hotmail.com>
2025-05-22 14:43:31 +0200

    Actualizando URL automatica

--- js/utils/config-api-url.js
+++ js/utils/config-api-url.js
@@ -2,5 +2,5 @@
 window.CONFIG = {
     //API_BASE_URL: "https://localhost:7187"  //local
     //API_BASE_URL: "https://formulariowebbackendapi.onrender.com" //url fija de Render
-    API_BASE_URL: "https://dos-cant-infectious-bandwidth.trycloudflare.com"  //url dinamica de cloudflare
+    API_BASE_URL: "https://encouraged-put-frontier-congress.trycloudflare.com"  //url dinamica de cloudflare
 };
