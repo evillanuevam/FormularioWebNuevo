8fc76edac14fab0e94baa82b17da4841d5b49d7e
Efrain <villanueva.efrain@hotmail.com>
2025-05-29 07:51:38 +0200

    Actualizando URL automatica

--- js/utils/config-api-url.js
+++ js/utils/config-api-url.js
@@ -2,5 +2,5 @@
 window.CONFIG = {
     //API_BASE_URL: "https://localhost:7187"  //local
     //API_BASE_URL: "https://formulariowebbackendapi.onrender.com" //url fija de Render
-    API_BASE_URL: "https://isp-france-aimed-glasgow.trycloudflare.com"  //url dinamica de cloudflare
+    API_BASE_URL: "https://effects-bermuda-portions-villa.trycloudflare.com"  //url dinamica de cloudflare
 };
