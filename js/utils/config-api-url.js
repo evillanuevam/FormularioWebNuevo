801d115d51c1c9c3fb3b832807ee888347945351
Efrain <villanueva.efrain@hotmail.com>
2025-06-24 08:32:04 +0200

    Actualizando URL automatica

--- js/utils/config-api-url.js
+++ js/utils/config-api-url.js
@@ -2,5 +2,5 @@
 window.CONFIG = {
     //API_BASE_URL: "https://localhost:7187"  //local
     //API_BASE_URL: "https://formulariowebbackendapi.onrender.com" //url fija de Render
-    API_BASE_URL: "https://skilled-behavioral-morning-foundation.trycloudflare.com"  //url dinamica de cloudflare
+    API_BASE_URL: "https://noise-ideal-ram-ampland.trycloudflare.com"  //url dinamica de cloudflare
 };
