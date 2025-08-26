3506b1e74634b431588c5fd527a637c99abbc2d5
Efrain <villanueva.efrain@hotmail.com>
2025-08-26 10:57:14 +0200

    Actualizando URL automatica

--- js/utils/config-api-url.js
+++ js/utils/config-api-url.js
@@ -2,5 +2,5 @@
 window.CONFIG = {
     //API_BASE_URL: "https://localhost:7187"  //local
     //API_BASE_URL: "https://formulariowebbackendapi.onrender.com" //url fija de Render
-    API_BASE_URL: "https://herbal-lafayette-pdas-confirmed.trycloudflare.com"  //url dinamica de cloudflare
+    API_BASE_URL: "https://concentration-sound-naval-merge.trycloudflare.com"  //url dinamica de cloudflare
 };
