4aa7117bbc1b26b1abda11be91b5dc97c23e8fe5
Efrain <villanueva.efrain@hotmail.com>
2025-09-15 09:34:04 +0200

    Actualizando URL automatica

--- js/utils/config-api-url.js
+++ js/utils/config-api-url.js
@@ -2,5 +2,5 @@
 window.CONFIG = {
     //API_BASE_URL: "https://localhost:7187"  //local
     //API_BASE_URL: "https://formulariowebbackendapi.onrender.com" //url fija de Render
-    API_BASE_URL: "https://depth-warcraft-perth-round.trycloudflare.com"  //url dinamica de cloudflare
+    API_BASE_URL: "https://adjustments-local-cannon-bite.trycloudflare.com"  //url dinamica de cloudflare
 };
