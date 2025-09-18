b0fc9ca69f9853343f252e5c45fbbebc3e1d417c
Efrain <villanueva.efrain@hotmail.com>
2025-09-18 07:59:41 +0200

    Actualizando URL automatica

--- js/utils/config-api-url.js
+++ js/utils/config-api-url.js
@@ -2,5 +2,5 @@
 window.CONFIG = {
     //API_BASE_URL: "https://localhost:7187"  //local
     //API_BASE_URL: "https://formulariowebbackendapi.onrender.com" //url fija de Render
-    API_BASE_URL: "https://soa-casio-examines-expand.trycloudflare.com"  //url dinamica de cloudflare
+    API_BASE_URL: "https://sensitive-associated-exhibitions-structures.trycloudflare.com"  //url dinamica de cloudflare
 };
