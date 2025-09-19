4c652b7459069347ffb9d8f23656ec545142d8fe
Efrain <villanueva.efrain@hotmail.com>
2025-09-19 08:35:45 +0200

    Actualizando URL automatica

--- js/utils/config-api-url.js
+++ js/utils/config-api-url.js
@@ -2,5 +2,5 @@
 window.CONFIG = {
     //API_BASE_URL: "https://localhost:7187"  //local
     //API_BASE_URL: "https://formulariowebbackendapi.onrender.com" //url fija de Render
-    API_BASE_URL: "https://sensitive-associated-exhibitions-structures.trycloudflare.com"  //url dinamica de cloudflare
+    API_BASE_URL: "https://parenting-wars-signs-tunnel.trycloudflare.com"  //url dinamica de cloudflare
 };
