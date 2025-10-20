ca3a98b63b52da8aac643e8b0886253736494f68
Efrain <villanueva.efrain@hotmail.com>
2025-10-20 08:55:59 +0200

    Actualizando URL automatica

--- js/utils/config-api-url.js
+++ js/utils/config-api-url.js
@@ -2,5 +2,5 @@
 window.CONFIG = {
     //API_BASE_URL: "https://localhost:7187"  //local
     //API_BASE_URL: "https://formulariowebbackendapi.onrender.com" //url fija de Render
-    API_BASE_URL: "https://teens-household-webshots-provider.trycloudflare.com"  //url dinamica de cloudflare
+    API_BASE_URL: "https://rob-essentially-replica-especially.trycloudflare.com"  //url dinamica de cloudflare
 };
