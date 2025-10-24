4ff385d81df48023e10fd63849fede7dc974dbfd
Efrain <villanueva.efrain@hotmail.com>
2025-10-24 08:09:49 +0200

    Actualizando URL automatica

--- js/utils/config-api-url.js
+++ js/utils/config-api-url.js
@@ -2,5 +2,5 @@
 window.CONFIG = {
     //API_BASE_URL: "https://localhost:7187"  //local
     //API_BASE_URL: "https://formulariowebbackendapi.onrender.com" //url fija de Render
-    API_BASE_URL: "https://cigarettes-plant-carey-ownership.trycloudflare.com"  //url dinamica de cloudflare
+    API_BASE_URL: "https://provided-roommate-building-scout.trycloudflare.com"  //url dinamica de cloudflare
 };
