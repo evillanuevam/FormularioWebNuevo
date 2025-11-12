fab65a0585f4631d6c7e2fe9b74065082e14e598
Efrain <villanueva.efrain@hotmail.com>
2025-11-12 08:03:31 +0100

    Actualizando URL automatica

--- js/utils/config-api-url.js
+++ js/utils/config-api-url.js
@@ -2,5 +2,5 @@
 window.CONFIG = {
     //API_BASE_URL: "https://localhost:7187"  //local
     //API_BASE_URL: "https://formulariowebbackendapi.onrender.com" //url fija de Render
-    API_BASE_URL: "https://margin-findarticles-score-gnome.trycloudflare.com"  //url dinamica de cloudflare
+    API_BASE_URL: "https://clarke-display-nerve-accurate.trycloudflare.com"  //url dinamica de cloudflare
 };
