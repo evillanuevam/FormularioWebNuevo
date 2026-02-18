bc457cc8937c323a421cbe9eedcbaf499ba60e33
Efrain <villanueva.efrain@hotmail.com>
2026-02-18 09:15:35 +0100

    Actualizando URL automatica

--- js/utils/config-api-url.js
+++ js/utils/config-api-url.js
@@ -2,5 +2,5 @@
 window.CONFIG = {
     //API_BASE_URL: "https://localhost:7187"  //local
     //API_BASE_URL: "https://formulariowebbackendapi.onrender.com" //url fija de Render
-    API_BASE_URL: "https://subtle-geographic-assume-him.trycloudflare.com"  //url dinamica de cloudflare
+    API_BASE_URL: "https://closes-evans-alpha-breeding.trycloudflare.com"  //url dinamica de cloudflare
 };
