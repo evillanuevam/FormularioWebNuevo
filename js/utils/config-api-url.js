ea41add84d31d0d51a1455a566d35fdc1e16202e
Efrain <villanueva.efrain@hotmail.com>
2026-02-25 10:18:04 +0100

    Actualizando URL automatica

--- js/utils/config-api-url.js
+++ js/utils/config-api-url.js
@@ -2,5 +2,5 @@
 window.CONFIG = {
     //API_BASE_URL: "https://localhost:7187"  //local
     //API_BASE_URL: "https://formulariowebbackendapi.onrender.com" //url fija de Render
-    API_BASE_URL: "https://omaha-commented-goat-thank.trycloudflare.com"  //url dinamica de cloudflare
+    API_BASE_URL: "https://elsewhere-dance-designated-does.trycloudflare.com"  //url dinamica de cloudflare
 };
