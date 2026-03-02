93f0aa8ce0d0c5ba4be5f4d007f26f9451bc96cc
Efrain <villanueva.efrain@hotmail.com>
2026-03-02 09:41:35 +0100

    Actualizando URL automatica

--- js/utils/config-api-url.js
+++ js/utils/config-api-url.js
@@ -2,5 +2,5 @@
 window.CONFIG = {
     //API_BASE_URL: "https://localhost:7187"  //local
     //API_BASE_URL: "https://formulariowebbackendapi.onrender.com" //url fija de Render
-    API_BASE_URL: "https://either-these-distinction-surprised.trycloudflare.com"  //url dinamica de cloudflare
+    API_BASE_URL: "https://betty-bradford-complexity-ambassador.trycloudflare.com"  //url dinamica de cloudflare
 };
