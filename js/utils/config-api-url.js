4d7707a89c5bac52e854f881e656ad88293c7c38
Efrain <villanueva.efrain@hotmail.com>
2026-03-17 13:16:37 +0100

    Actualizando URL automatica

--- js/utils/config-api-url.js
+++ js/utils/config-api-url.js
@@ -2,5 +2,5 @@
 window.CONFIG = {
     //API_BASE_URL: "https://localhost:7187"  //local
     //API_BASE_URL: "https://formulariowebbackendapi.onrender.com" //url fija de Render
-    API_BASE_URL: "https://exceptions-varies-beach-commission.trycloudflare.com"  //url dinamica de cloudflare
+    API_BASE_URL: "https://shaved-johnston-approx-open.trycloudflare.com"  //url dinamica de cloudflare
 };
