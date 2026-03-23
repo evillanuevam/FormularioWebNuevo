43a64dd87d90bc4fbe28713c66546c96316c10f5
Efrain <villanueva.efrain@hotmail.com>
2026-03-23 09:24:52 +0100

    Actualizando URL automatica

--- js/utils/config-api-url.js
+++ js/utils/config-api-url.js
@@ -2,5 +2,5 @@
 window.CONFIG = {
     //API_BASE_URL: "https://localhost:7187"  //local
     //API_BASE_URL: "https://formulariowebbackendapi.onrender.com" //url fija de Render
-    API_BASE_URL: "https://environmental-plenty-enjoying-finest.trycloudflare.com"  //url dinamica de cloudflare
+    API_BASE_URL: "https://argued-vegetable-rss-mods.trycloudflare.com"  //url dinamica de cloudflare
 };
