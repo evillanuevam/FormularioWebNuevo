93a1ea07c5359fcd733b89b2550bb588afb04f36
Efrain <villanueva.efrain@hotmail.com>
2026-03-25 09:09:42 +0100

    Actualizando URL automatica

--- js/utils/config-api-url.js
+++ js/utils/config-api-url.js
@@ -2,5 +2,5 @@
 window.CONFIG = {
     //API_BASE_URL: "https://localhost:7187"  //local
     //API_BASE_URL: "https://formulariowebbackendapi.onrender.com" //url fija de Render
-    API_BASE_URL: "https://productions-approved-extra-cancer.trycloudflare.com"  //url dinamica de cloudflare
+    API_BASE_URL: "https://expenditure-ross-smile-verification.trycloudflare.com"  //url dinamica de cloudflare
 };
