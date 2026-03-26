24718593b572053254c6dbaa26caf2bb814721dd
Efrain <villanueva.efrain@hotmail.com>
2026-03-26 13:57:04 +0100

    Actualizando URL automatica

--- js/utils/config-api-url.js
+++ js/utils/config-api-url.js
@@ -2,5 +2,5 @@
 window.CONFIG = {
     //API_BASE_URL: "https://localhost:7187"  //local
     //API_BASE_URL: "https://formulariowebbackendapi.onrender.com" //url fija de Render
-    API_BASE_URL: "https://knee-veteran-folder-managing.trycloudflare.com"  //url dinamica de cloudflare
+    API_BASE_URL: "https://sticky-tiles-outsourcing-contractors.trycloudflare.com"  //url dinamica de cloudflare
 };
