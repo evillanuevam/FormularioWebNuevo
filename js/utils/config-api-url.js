171bc5e0413ee9124a4e221170b3395e798353e3
Efrain <villanueva.efrain@hotmail.com>
2026-03-30 09:01:21 +0200

    Actualizando URL automatica

--- js/utils/config-api-url.js
+++ js/utils/config-api-url.js
@@ -2,5 +2,5 @@
 window.CONFIG = {
     //API_BASE_URL: "https://localhost:7187"  //local
     //API_BASE_URL: "https://formulariowebbackendapi.onrender.com" //url fija de Render
-    API_BASE_URL: "https://patrol-logan-munich-prisoners.trycloudflare.com"  //url dinamica de cloudflare
+    API_BASE_URL: "https://long-teacher-small-except.trycloudflare.com"  //url dinamica de cloudflare
 };
