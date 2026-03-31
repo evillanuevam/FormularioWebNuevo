f71f7a8d431a8baf0d4c5eaea6538e540601e8ba
Efrain <villanueva.efrain@hotmail.com>
2026-03-31 08:16:42 +0200

    Actualizando URL automatica

--- js/utils/config-api-url.js
+++ js/utils/config-api-url.js
@@ -2,5 +2,5 @@
 window.CONFIG = {
     //API_BASE_URL: "https://localhost:7187"  //local
     //API_BASE_URL: "https://formulariowebbackendapi.onrender.com" //url fija de Render
-    API_BASE_URL: "https://long-teacher-small-except.trycloudflare.com"  //url dinamica de cloudflare
+    API_BASE_URL: "https://made-teeth-exhibits-purchasing.trycloudflare.com"  //url dinamica de cloudflare
 };
