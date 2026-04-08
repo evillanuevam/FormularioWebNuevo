3e853bd9dc725d935a17fc814806533f432949d0
Efrain <villanueva.efrain@hotmail.com>
2026-04-08 09:09:53 +0200

    Actualizando URL automatica

--- js/utils/config-api-url.js
+++ js/utils/config-api-url.js
@@ -2,5 +2,5 @@
 window.CONFIG = {
     //API_BASE_URL: "https://localhost:7187"  //local
     //API_BASE_URL: "https://formulariowebbackendapi.onrender.com" //url fija de Render
-    API_BASE_URL: "https://massachusetts-collector-leo-tions.trycloudflare.com"  //url dinamica de cloudflare
+    API_BASE_URL: "https://representatives-deaf-pendant-theft.trycloudflare.com"  //url dinamica de cloudflare
 };
