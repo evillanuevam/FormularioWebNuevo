275225dcecd8483df2a154f63df5144c21f30982
Efrain <villanueva.efrain@hotmail.com>
2026-04-15 08:50:03 +0200

    Actualizando URL automatica

--- js/utils/config-api-url.js
+++ js/utils/config-api-url.js
@@ -2,5 +2,5 @@
 window.CONFIG = {
     //API_BASE_URL: "https://localhost:7187"  //local
     //API_BASE_URL: "https://formulariowebbackendapi.onrender.com" //url fija de Render
-    API_BASE_URL: "https://delayed-sic-treasure-ooo.trycloudflare.com"  //url dinamica de cloudflare
+    API_BASE_URL: "https://billy-condo-entry-wireless.trycloudflare.com"  //url dinamica de cloudflare
 };
