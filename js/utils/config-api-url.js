7a00f2ed85593a66c0eb281f3cfa45fb79c72b6a
Efrain <villanueva.efrain@hotmail.com>
2026-04-24 09:19:54 +0200

    Actualizando URL automatica

--- js/utils/config-api-url.js
+++ js/utils/config-api-url.js
@@ -2,5 +2,5 @@
 window.CONFIG = {
     //API_BASE_URL: "https://localhost:7187"  //local
     //API_BASE_URL: "https://formulariowebbackendapi.onrender.com" //url fija de Render
-    API_BASE_URL: "https://pharmacology-pull-rise-activated.trycloudflare.com"  //url dinamica de cloudflare
+    API_BASE_URL: "https://techno-quantity-currencies-continent.trycloudflare.com"  //url dinamica de cloudflare
 };
