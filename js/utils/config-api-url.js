8a247730253492a5ee48c24579e7ce808b588d12
Efrain <villanueva.efrain@hotmail.com>
2026-04-29 09:32:57 +0200

    Actualizando URL automatica

--- js/utils/config-api-url.js
+++ js/utils/config-api-url.js
@@ -2,5 +2,5 @@
 window.CONFIG = {
     //API_BASE_URL: "https://localhost:7187"  //local
     //API_BASE_URL: "https://formulariowebbackendapi.onrender.com" //url fija de Render
-    API_BASE_URL: "https://sector-moreover-home-madonna.trycloudflare.com"  //url dinamica de cloudflare
+    API_BASE_URL: "https://pathology-boom-meaning-begins.trycloudflare.com"  //url dinamica de cloudflare
 };
