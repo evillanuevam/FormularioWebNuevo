be2d84c9a88c470a1cda4e5917f727da734c3164
Efrain <villanueva.efrain@hotmail.com>
2026-05-07 08:09:38 +0200

    Actualizando URL automatica

--- js/utils/config-api-url.js
+++ js/utils/config-api-url.js
@@ -2,5 +2,5 @@
 window.CONFIG = {
     //API_BASE_URL: "https://localhost:7187"  //local
     //API_BASE_URL: "https://formulariowebbackendapi.onrender.com" //url fija de Render
-    API_BASE_URL: "https://edition-broadcast-district-psychiatry.trycloudflare.com"  //url dinamica de cloudflare
+    API_BASE_URL: "https://band-licence-deleted-unlike.trycloudflare.com"  //url dinamica de cloudflare
 };
