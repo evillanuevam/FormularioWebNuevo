1a20e9bfa3851ed33d3d5e1c69a537f6928a06a5
Efrain <villanueva.efrain@hotmail.com>
2026-05-07 11:10:56 +0200

    Actualizando URL automatica

--- js/utils/config-api-url.js
+++ js/utils/config-api-url.js
@@ -2,5 +2,5 @@
 window.CONFIG = {
     //API_BASE_URL: "https://localhost:7187"  //local
     //API_BASE_URL: "https://formulariowebbackendapi.onrender.com" //url fija de Render
-    API_BASE_URL: "https://band-licence-deleted-unlike.trycloudflare.com"  //url dinamica de cloudflare
+    API_BASE_URL: "https://someone-refuse-buses-enterprises.trycloudflare.com"  //url dinamica de cloudflare
 };
