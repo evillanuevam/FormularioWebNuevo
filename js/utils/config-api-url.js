d2c1e991ba47b47c5cb18523254e8c3943199add
Efrain <villanueva.efrain@hotmail.com>
2026-05-11 09:21:53 +0200

    Actualizando URL automatica

--- js/utils/config-api-url.js
+++ js/utils/config-api-url.js
@@ -2,5 +2,5 @@
 window.CONFIG = {
     //API_BASE_URL: "https://localhost:7187"  //local
     //API_BASE_URL: "https://formulariowebbackendapi.onrender.com" //url fija de Render
-    API_BASE_URL: "https://seconds-accomplished-sufficiently-jamie.trycloudflare.com"  //url dinamica de cloudflare
+    API_BASE_URL: "https://mono-skiing-actual-rooms.trycloudflare.com"  //url dinamica de cloudflare
 };
