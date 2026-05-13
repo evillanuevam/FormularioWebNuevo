db4e80e55d9216570efe94dbfd97425eea57bc1f
Efrain <villanueva.efrain@hotmail.com>
2026-05-13 11:26:54 +0200

    Actualizando URL automatica

--- js/utils/config-api-url.js
+++ js/utils/config-api-url.js
@@ -2,5 +2,5 @@
 window.CONFIG = {
     //API_BASE_URL: "https://localhost:7187"  //local
     //API_BASE_URL: "https://formulariowebbackendapi.onrender.com" //url fija de Render
-    API_BASE_URL: "https://accuracy-sending-homework-shopping.trycloudflare.com"  //url dinamica de cloudflare
+    API_BASE_URL: "https://seattle-castle-humanity-philadelphia.trycloudflare.com"  //url dinamica de cloudflare
 };
