982519b13cf5e2b2af55bbbf09f2d5e41a470fdf
Efrain <villanueva.efrain@hotmail.com>
2026-05-21 09:23:14 +0200

    Actualizando URL automatica

--- js/utils/config-api-url.js
+++ js/utils/config-api-url.js
@@ -2,5 +2,5 @@
 window.CONFIG = {
     //API_BASE_URL: "https://localhost:7187"  //local
     //API_BASE_URL: "https://formulariowebbackendapi.onrender.com" //url fija de Render
-    API_BASE_URL: "https://remix-oasis-redhead-project.trycloudflare.com"  //url dinamica de cloudflare
+    API_BASE_URL: "https://replies-hugh-senior-bicycle.trycloudflare.com"  //url dinamica de cloudflare
 };
