3c655c22a1e98d19e94867e5bd9384eaf8480f85
Efrain <villanueva.efrain@hotmail.com>
2026-06-01 11:07:29 +0200

    Actualizando URL automatica

--- js/utils/config-api-url.js
+++ js/utils/config-api-url.js
@@ -2,5 +2,5 @@
 window.CONFIG = {
     //API_BASE_URL: "https://localhost:7187"  //local
     //API_BASE_URL: "https://formulariowebbackendapi.onrender.com" //url fija de Render
-    API_BASE_URL: "https://guarantees-boxed-tribes-collect.trycloudflare.com"  //url dinamica de cloudflare
+    API_BASE_URL: "https://excerpt-duke-feel-bullet.trycloudflare.com"  //url dinamica de cloudflare
 };
