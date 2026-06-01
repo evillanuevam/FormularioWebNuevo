17a2870c7869eeb8373a9c99801bb9bbed3202c6
Efrain <villanueva.efrain@hotmail.com>
2026-06-01 15:18:02 +0200

    Actualizando URL automatica

--- js/utils/config-api-url.js
+++ js/utils/config-api-url.js
@@ -2,5 +2,5 @@
 window.CONFIG = {
     //API_BASE_URL: "https://localhost:7187"  //local
     //API_BASE_URL: "https://formulariowebbackendapi.onrender.com" //url fija de Render
-    API_BASE_URL: "https://serve-rather-visible-seniors.trycloudflare.com"  //url dinamica de cloudflare
+    API_BASE_URL: "https://refers-plastics-forestry-tex.trycloudflare.com"  //url dinamica de cloudflare
 };
