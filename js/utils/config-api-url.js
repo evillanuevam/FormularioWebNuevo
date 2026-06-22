9faadd8b262ccafab66eb302c964bedf41b5e0ef
Efrain <villanueva.efrain@hotmail.com>
2026-06-22 08:18:06 +0200

    Actualizando URL automatica

--- js/utils/config-api-url.js
+++ js/utils/config-api-url.js
@@ -2,5 +2,5 @@
 window.CONFIG = {
     //API_BASE_URL: "https://localhost:7187"  //local
     //API_BASE_URL: "https://formulariowebbackendapi.onrender.com" //url fija de Render
-    API_BASE_URL: "https://scanner-rover-unavailable-immigration.trycloudflare.com"  //url dinamica de cloudflare
+    API_BASE_URL: "https://barnes-damaged-aka-klein.trycloudflare.com"  //url dinamica de cloudflare
 };
