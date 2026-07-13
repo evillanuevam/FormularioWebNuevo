140ed12a2a310855fb3d7dbf10cdcf558dbd8ca2
Efrain <villanueva.efrain@hotmail.com>
2026-07-13 14:06:18 +0200

    Actualizando URL automatica

--- js/utils/config-api-url.js
+++ js/utils/config-api-url.js
@@ -2,5 +2,5 @@
 window.CONFIG = {
     //API_BASE_URL: "https://localhost:7187"  //local
     //API_BASE_URL: "https://formulariowebbackendapi.onrender.com" //url fija de Render
-    API_BASE_URL: "https://capability-signatures-mitsubishi-isle.trycloudflare.com"  //url dinamica de cloudflare
+    API_BASE_URL: "https://details-promoting-one-orange.trycloudflare.com"  //url dinamica de cloudflare
 };
