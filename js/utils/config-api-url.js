1ea389cdf327a1a6c3a06743b2ad091dce9c0ab2
Efrain <villanueva.efrain@hotmail.com>
2026-07-16 12:40:52 +0200

    Actualizando URL automatica

--- js/utils/config-api-url.js
+++ js/utils/config-api-url.js
@@ -2,5 +2,5 @@
 window.CONFIG = {
     //API_BASE_URL: "https://localhost:7187"  //local
     //API_BASE_URL: "https://formulariowebbackendapi.onrender.com" //url fija de Render
-    API_BASE_URL: "https://fresh-neil-discrete-hearts.trycloudflare.com"  //url dinamica de cloudflare
+    API_BASE_URL: "https://scripts-laws-board-bright.trycloudflare.com"  //url dinamica de cloudflare
 };
